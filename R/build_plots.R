ac46d30e7ccd9327c6d675f01a31eef35c87a5c8
Taylor Spears <tcs@tcspears.net>
2015-02-22 19:16:12 +0000

    Added new option tofile to build_plots

--- R/build_plots.R
+++ R/build_plots.R
@@ -2,7 +2,7 @@
 #' 
 #' @param date (optional) character vector of dates, can either be of length 1, 2, or left unspecified. If length(date)==1, then the plots will include all commits from that day until the latest commit. If length(date)==2, then date will be treated as a range, and the plot will include all commits within that range. If date is left unspecified, then the plot will include all commits in the repository.
 #' @param repo (optional) character string giving the relative or absolute path to the git repository. If left unspecified, the current working directory will be used as the repo.
-#' @return Nothing returned - plots are saved to the directory specified in repo as 'wordplot.pdf'
+#' @param tofile (optional) logical specifying whether the plot should be written to a pdf file (instead of displayed interactively)
 #' @examples
 #' build_plots()
 #' build_plots(date="2015-01-01")
@@ -10,7 +10,7 @@
 #' build_plots(date=c("2015-01-01","2015-02-20"))
 #' build_plots(date=c("2015-01-01","2015-02-20"),repo="~/repositories/project/")
 
-build_plots <- function(date=NULL,repo=getwd()){
+build_plots <- function(date=NULL,repo=getwd(),tofile=FALSE){
   a <- extract_commits(repo)
   b <- NULL
   if(is.null(date)){
@@ -24,15 +24,19 @@ build_plots <- function(date=NULL,repo=getwd()){
   }
   c <- add_word_counts_table(b,repo)
   d <- collapse_date(c)
-  if(substr(repo,nchar(repo),nchar(repo))=="/"){
-    pdf(file=paste(repo,"wordplot.pdf",sep=""))
+  if(tofile==FALSE){
     plot_words(d)
-    dev.off()
-    cat("Plot saved as wordplot.pdf within ",repo,"\n")
   } else {
-    pdf(file=paste(repo,"/wordplot.pdf",sep=""))
-    plot_words(d)
-    dev.off()
-    cat("Plot saved as wordplot.pdf within ",repo,"\n")
+    if(substr(repo,nchar(repo),nchar(repo))=="/"){
+      pdf(file=paste(repo,"wordplot.pdf",sep=""))
+      plot_words(d)
+      dev.off()
+      cat("Plot saved as wordplot.pdf within ",repo,"\n")
+    } else {
+      pdf(file=paste(repo,"/wordplot.pdf",sep=""))
+      plot_words(d)
+      dev.off()
+      cat("Plot saved as wordplot.pdf within ",repo,"\n")
+    }
   }
 }
