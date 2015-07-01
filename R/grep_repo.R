48afb22cf268a5b190e6de8c89306aea5979d1fe
Taylor Spears <tcs@tcspears.net>
2015-07-01 11:04:48 +0100

    Added comments to improve readability

--- R/grep_repo.R
+++ R/grep_repo.R
@@ -7,9 +7,16 @@
 #' @return A character vector of results.
 
 grep_repo <- function(expression,repo=getwd()){
+  # Grep entire repo for expression and then save output to 'a'
   a <- system(paste(git.location," grep ","\'",expression,"\' ","$(git rev-list --all)",sep=""),intern=TRUE)
+  
+  # Cut the git commit ID from output to enable comparison of duplicates
   b <- substr(a,42,nchar(a))
+  
+  # Identify and drop duplicates
   dup <- duplicated(b)
   c <- b[!dup]
+  
+  # Return output
   return(c)
 }
